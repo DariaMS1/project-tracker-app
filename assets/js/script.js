91909cd69341acabaedbb87bbe4d849f1037e55b
daria <darias2002@gmail.com>
2024-03-22 17:33:06 +0000

    colours display and storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 var currentDaySelector = $('#currentDay');
 var blocksContainer = $('#timeBlocks');
 var timeBlocks = $('.time-block')
-var selectedTimeBlock
 var saveBtn = $('#saveBtn')
 var block = $(``)
 
@@ -10,3 +9,31 @@ function printTime() {
     currentDaySelector.text(currentDayDisplay);
 }
 setInterval(printTime, 1000);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+      let inputValue = $(this).siblings(".description").val();
+      let keyValue = $(this).parent().attr("id");
+      console.log(inputValue, keyValue);
+      localStorage.setItem(keyValue, inputValue);
+    });
+    console.log($(this));
+  
+    $(".time-block").each(function () {
+      let hourBlock = parseInt($(this).attr("id").split("-").pop());
+      let time = dayjs().hour();
+  
+      if (hourBlock < time) {
+        $(this).addClass('past');
+      }
+      else if (hourBlock > time) {
+        $(this).addClass('future');
+      }
+      else {
+        $(this).addClass('present');
+      }
+      
+      let key = $(this).attr("id");
+      $(this).children(".description").val(localStorage.getItem(key));
+    });
+});
